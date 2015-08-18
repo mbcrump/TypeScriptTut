da57066fe34a5e6d8313df6c133bcdcc68fb81e4
mbcrump <mbcrump29@gmail.com>
2015-08-18 15:29:20 -0500

    Updates

---
+++ TypeScriptTutorial/Scripts/03.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="typings/jquery/jquery.d.ts" />
+
+declare var $: JQueryStatic;
+
+var onClicked = function () {
+    alert("Clicked!");
+}
+
+var main = function () {
+    $("#greet").click(onClicked);
+}
+
+$(document).ready(function () {
+    main();
+});
